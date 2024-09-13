3bf040b17bd3922639abaaf9ea1a73f30a3071cd
demarlik01 <hyoseockkim@gmail.com>
2024-09-13 17:10:45 +0900

    Adjust styles

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -9,25 +9,24 @@ type Props = {
 };
 
 const CoverImage = ({ title, src, slug }: Props) => {
-  const image = (
-    <Image
-      src={src}
-      alt={`Cover Image for ${title}`}
-      className={cn("shadow-sm w-full", {
-        "hover:shadow-lg transition-shadow duration-200": slug,
-      })}
-      width={1300}
-      height={630}
-    />
-  );
   return (
     <div className="sm:mx-0">
       {slug ? (
         <Link href={`/posts/${slug}`} aria-label={title}>
-          {image}
+          <Image
+            src={src}
+            alt={`Cover Image for ${title}`}
+            className="duration-200 object-cover"
+            unoptimized
+          />
         </Link>
       ) : (
-        image
+        <Image
+          src={src}
+          alt={`Cover Image for ${title}`}
+          className="w-full h-auto"
+          unoptimized
+        />
       )}
     </div>
   );
