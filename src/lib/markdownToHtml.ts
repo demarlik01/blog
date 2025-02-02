b0043da2efc568ae9eaed7ac574daeee818f38ad
demarlik01 <hyoseockkim@gmail.com>
2025-02-02 15:59:21 +0900

    Add: remark-break for newline

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import { remark } from "remark";
 import html from "remark-html";
+import remarkBreaks from 'remark-breaks';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(remarkBreaks).use(html).process(markdown);
   return result.toString();
 }
